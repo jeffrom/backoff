6b6c400c5726479262a39b892292a9b5c62225cb
Jeff Martin <jeffmartin@gmail.com>
2017-09-17 16:43:58 -0400

    add a tools i've been using

---
+++ tools/ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euxo pipefail
+
+# script to run ci tasks
+
+mix dogma
+dogma_res=$?
+
+mix credo --strict
+credo_res=$?
+
+mix test
+test_res=$?
+
+mix dialyzer --halt-exit-status
+dialyzer_res=$?
+
+
+if [[ $dogma_res != 0 || $credo_res != 0 || $test_res != 0 || $dialyzer_res != 0 ]]; then
+    exit 1
+fi
